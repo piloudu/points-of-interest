445d6a5798a42bee4f393a6b78658ecbe42234d5
Xoan Gonzalez <xoangon@gmail.com>
2022-04-24 12:19:02 +0200

    Generate app Topbar

--- app/src/main/java/com/example/pointsofinterest/utils/UiUtils.kt
+++ app/src/main/java/com/example/pointsofinterest/utils/UiUtils.kt
@@ -3,8 +3,12 @@ package com.example.pointsofinterest.utils
 import android.app.Activity
 import android.content.Context
 import android.content.ContextWrapper
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.ui.Alignment
@@ -12,7 +16,14 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.pointsofinterest.R
+import com.example.pointsofinterest.ui.theme.DarkGray
+import com.example.pointsofinterest.ui.theme.LightGray
 
 @Composable
 fun LoadingIndicator(
@@ -37,17 +48,46 @@ fun LoadingIndicator(
     }
 }
 
+@Preview
 @Composable
 fun TopBar(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier.background(LightGray),
+    backAction: () -> Unit = {}
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
             .height(44.dp)
-            .padding(start = 15.dp, end = 15.dp)
+            .padding(start = 15.dp, end = 15.dp, bottom = 13.dp)
+            .background(LightGray)
     ) {
-
+        Image(
+            modifier = Modifier.clickable {
+                backAction()
+            },
+            painter = painterResource(id = R.drawable.ic_back_arrow),
+            contentDescription = "back arrow"
+        )
+        Spacer(modifier = Modifier.width(32.dp))
+        Text(
+            modifier = Modifier
+                .width(199.dp),
+            text = "MADRID",
+            color = DarkGray,
+            letterSpacing = 0.78.sp,
+            lineHeight = 16.sp,
+            fontSize = 25.sp
+        )
+        Spacer(modifier = Modifier.width(20.dp))
+        Image(
+            painter = painterResource(id = R.drawable.ic_calendar),
+            contentDescription = "calendar"
+        )
+        Spacer(modifier = Modifier.width(5.dp))
+        Image(
+            painter = painterResource(id = R.drawable.ic_arrow),
+            contentDescription = "arrow"
+        )
     }
 }
 
